a62e4b07fbce8ef92d69af50c5070c7a182fb9a9
Jesus Agudo <jagudo2514@gmail.com>
2023-12-15 00:03:28 -0500

    Add conducting and presentation sections to navigationsProject.js

--- src/app/navigationsProject.js
+++ src/app/navigationsProject.js
@@ -1,3 +1,5 @@
+
+/* Systematic literature review - Tool for researchers */
 export const navigationsProject = [
   { name: '#Name Project', icon: 'book', path: '/projects/new' },
   { label: 'Steps', type: 'label' },
@@ -5,34 +7,35 @@ export const navigationsProject = [
     name: 'Planning',
     icon: 'playlist_add_check',
     children: [
-      { name: 'Planning', path: '/projects/new/planning' },
-      { name: 'Planning', path: '/projects/new/planning' },
-      { name: 'Planning', path: '/projects/new/planning' },
+      { name: 'General', path: '/projects/new/planning' },
+      { name: 'Review protocol', path: '/projects/new/planning' },
+      { name: 'Quality criteria', path: '/projects/new/planning' },
+      { name: 'Timeline', path: '/projects/new/planning' },
+    ]
+  },
+  {
+    name: 'Conducting',
+    icon: 'assignment',
+    children: [
+      { name: 'Search', path: '/projects/new/conducting' },
+      { name: 'Selection', path: '/projects/new/conducting' },
+      { name: 'Data extraction', path: '/projects/new/conducting' },
+      { name: 'Quality assessment', path: '/projects/new/conducting' },
+      { name: 'Data analysis', path: '/projects/new/conducting' },
     ]
   },
   {
     name: 'Presentation',
-    icon: 'slideshow',
+    icon: 'assignment_turned_in',
     children: [
-      { name: 'Presentation', path: '/projects/new/presentation' },
-      { name: 'Presentation', path: '/projects/new/presentation' },
-      { name: 'Presentation', path: '/projects/new/presentation' },
+      { name: 'Results', path: '/projects/new/presentation' },
+      { name: 'Visualization', path: '/projects/new/presentation' },
+      { name: 'Export', path: '/projects/new/presentation' },
     ]
   },
-  {
-    name: 'Execution',
-    icon: 'build',
-    children: [
-      { name: 'Execution', path: '/projects/new/execution' },
-      { name: 'Execution', path: '/projects/new/execution' },
-      { name: 'Execution', path: '/projects/new/execution' },
-    ]
-  },
-  { label: 'Support', type: 'label' },
-  {
-    name: 'Documentation',
-    icon: 'launch',
-    type: 'extLink',
-    path: ''
-  },
+  { label: 'Communication', type: 'label' },
+  { name: 'Chat', icon: 'chat', path: '/projects/new/chat' },
+  { name: 'Files', icon: 'folder', path: '/projects/new/files' },
+  { name: 'Kanban', icon: 'view_column', path: '/projects/new/kanban' },
+  
 ];
